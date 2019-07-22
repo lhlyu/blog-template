b18b339a4e7c4e13890752a7c1497d802054ffd5
lhlyu <lhl103512@163.com>
2019-07-22 18:23:16 +0800

    修改

---
+++ src/statics/local/js/article.js
@@ -0,0 +1,22 @@
+
+
+$(function() {
+    editormd.markdownToHTML("yu-article-content", {
+        htmlDecode      : "style,script,iframe",  // you can filter tags decode
+        emoji           : true,
+        taskList        : true,
+        tex             : true,  // 默认不解析
+        flowChart       : true,  // 默认不解析
+        sequenceDiagram : true,  // 默认不解析
+    });
+
+    var OwO_demo = new OwO({
+        logo: 'OωO表情',
+        container: document.getElementsByClassName('OwO')[0],
+        target: document.getElementsByClassName('OwO-textarea')[0],
+        api: './statics/third-party/owo/OwO.json',
+        position: 'up',
+        width: '100%',
+        maxHeight: '250px'
+    });
+})
